ac33f13445b77b8de1ca990613a2a45ff0d79dd3
Muhammed Rezk Rajab <mrezk319@gmail.com>
2025-04-24 22:06:52 +0200

    add welcome message

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to your course management!",
+  });
+});
 app.use("/api/courses", courseRouter);
 
 module.exports = app;
