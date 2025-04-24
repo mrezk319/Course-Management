ed295ec1195d8d2435e70f42161510dd1041fd06
Muhammed Rezk Rajab <mrezk319@gmail.com>
2025-04-25 01:15:31 +0300

    edits

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Curse Management Api!" });
+  res.status(200).json({ message: "Welcome to Course Management Api!" });
 });
 app.use("/api/courses", courseRouter);
 
